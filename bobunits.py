b94904969f94be9372eb6073d9ff9c88917c9a32
Sudeep Jathar <sudeepjathar@vertex.local>
2015-05-04 09:57:37 +0530

    Removed bob files

--- bobunits.py
+++
@@ -1,8 +0,0 @@
-#bobunits.py
-from unitz import unit, done
-
-@unit('add_numbers')
-def add(a, b):
-    o_addition_result = a + b
-    print '{0} + {1} = {2}'.format(a,b, o_addition_result)
-    return done()
